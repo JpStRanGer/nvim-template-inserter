989feb25dbb93ab602d3ea3b101dd155e06cfcf5
Jonas Pettersen <jonas.pettersen@rvmsystems.com>
2025-03-17 14:21:28 +0100

    adding update of lsp and treesitter after loading template

--- lua/template_inserter/init.lua
+++ lua/template_inserter/init.lua
@@ -3,49 +3,56 @@ local M = {}
 
 -- Function to print a greeting
 function M.hello()
-    print("Hello from Remote MyPlugin!")
+	print("Hello from Remote MyPlugin!")
 end
 
 M.config = {
-    keymap = "<leader>tt", -- user-defined key
-    templates = {
-        html = "template_inserter.templates.html",
-        css = "template_inserter.templates.css",
-        cpp = "template_inserter.templates.cpp",
-        arduino = "template_inserter.templates.arduino",
-        cmake = "template_inserter.templates.cmake",
-        qt = "template_inserter.templates.qt",
-    },
+	keymap = "<leader>tt", -- user-defined key
+	templates = {
+		html = "template_inserter.templates.html",
+		css = "template_inserter.templates.css",
+		cpp = "template_inserter.templates.cpp",
+		arduino = "template_inserter.templates.arduino",
+		cmake = "template_inserter.templates.cmake",
+		qt = "template_inserter.templates.qt",
+	},
 }
 
 -- Function to create a command
 function M.setup(opts)
-    M.config = vim.tbl_extend("force", M.config, opts or {})
+	M.config = vim.tbl_extend("force", M.config, opts or {})
 
-    -- Set test function as executble function in nvim. ( this is just for testing that the plugin works)
-    vim.api.nvim_create_user_command("SayHello", function()
-        M.hello()
-    end, {})
+	-- Set test function as executble function in nvim. ( this is just for testing that the plugin works)
+	vim.api.nvim_create_user_command("SayHello", function()
+		M.hello()
+	end, {})
 
-    vim.keymap.set("n", M.config.keymap, function()
-        print("keymap reconised.")
-        M.trigger()
-    end, { desc = "Insert template", noremap = true, silent = true })
+	vim.keymap.set("n", M.config.keymap, function()
+		print("keymap reconised.")
+		M.trigger()
+	end, { desc = "Insert template", noremap = true, silent = true })
 end
 
 -- Check if current buffer is empty
 local function buffer_is_empty()
-    local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-    return #lines == 1 and lines[1] == ""
+	local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+	return #lines == 1 and lines[1] == ""
 end
 
 -- Trigger template selection if file is empty
 function M.trigger()
-    if not buffer_is_empty() then
-        vim.notify("Buffer not empty.. (not a clean page)", vim.log.levels.WARN)
-        return
-    end
-    require("template_inserter.picker").open(M.config.templates)
+	if not buffer_is_empty() then
+		vim.notify("Buffer not empty.. (not a clean page)", vim.log.levels.WARN)
+		return
+	end
+	require("template_inserter.picker").open(M.config.templates)
+
+	-- After a short delay, re-apply filetype detection and syntax highlighting
+	vim.defer_fn(function()
+		vim.cmd("filetype detect")
+		vim.cmd("doautocmd BufRead")
+		vim.cmd("syntax enable") -- Optional: if needed
+	end, 50)
 end
 
 return M
--- lua/template_inserter/picker.lua
+++ lua/template_inserter/picker.lua
@@ -8,26 +8,35 @@ local conf = require("telescope.config").values
 local M = {}
 
 function M.open(templates)
-    local keys = vim.tbl_keys(templates)
+	local keys = vim.tbl_keys(templates)
 
-    pickers
-        .new({}, {
-            prompt_title = "Choose Template",
-            finder = finders.new_table({ results = keys }),
-            sorter = conf.generic_sorter({}),
-            attach_mappings = function(prompt_bufnr, _)
-                actions.select_default:replace(function()
-                    actions.close(prompt_bufnr)
-                    local selection = action_state.get_selected_entry().value
-                    local mod = require(templates[selection])
-                    local lines = mod.get()
-                    vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
-                    vim.notify("Inserted template: " .. selection, vim.log.levels.INFO)
-                end)
-                return true
-            end,
-        })
-        :find()
+	pickers
+		.new({}, {
+			prompt_title = "Choose Template",
+			finder = finders.new_table({ results = keys }),
+			sorter = conf.generic_sorter({}),
+			attach_mappings = function(prompt_bufnr, _)
+				actions.select_default:replace(function()
+					actions.close(prompt_bufnr)
+
+					local selection = action_state.get_selected_entry().value
+					local mod = require(templates[selection])
+					local lines = mod.get()
+
+					-- Insert template into buffer
+					vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+
+					-- Set filetype based on the template name (e.g., "html", "cpp")
+					vim.bo.filetype = selection
+
+					-- Trigger LSP and Tree-sitter (syntax highlighting, autocommands, etc.)
+					vim.cmd("filetype detect")
+					vim.cmd("doautocmd BufRead")
+				end)
+				return true
+			end,
+		})
+		:find()
 end
 
 return M
